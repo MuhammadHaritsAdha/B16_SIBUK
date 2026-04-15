aa656fed4a3c81390486abdd2c5141fcea5614c2
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-15 10:57:07 +0700

    setup FormTransaksi dan load buku data ke ComboBox

--- SIBUK/Form2.cs
+++ SIBUK/Form2.cs
@@ -1,1 +1,74 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SIBUK
+{
+    public partial class FormTransaksi : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        string connString = "Data Source=RITS;Initial Catalog=TokoBukuDB;Integrated Security=True";
+        string roleUser;
+        int userId;
+        public FormTransaksi(string role, int id)
+        {
+            InitializeComponent();
+            roleUser = role;
+            userId = id;
+        }
+
+        private void FormTransaksi_Load(object sender, EventArgs e)
+        {
+            if (roleUser != "admin")
+                btnLaporan.Visible = false;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                string query = "SELECT bukuId, judul, hargaSatuan FROM Buku";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                cbBuku.DataSource = dt;
+                cbBuku.DisplayMember = "judul";
+                cbBuku.ValueMember = "bukuId";
+            }
+
+            txtHarga.ReadOnly = true;
+
+            dgvTransaksi.Columns.Clear();
+
+            dgvTransaksi.Columns.Add("bukuId", "ID");
+            dgvTransaksi.Columns.Add("judul", "Judul");
+            dgvTransaksi.Columns.Add("harga", "Harga");
+            dgvTransaksi.Columns.Add("jumlah", "Jumlah");
+            dgvTransaksi.Columns.Add("subtotal", "Subtotal");
+
+            dgvTransaksi.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvTransaksi_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void cbBuku_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbBuku.SelectedItem is DataRowView row)
+            {
+                txtHarga.Text = row["hargaSatuan"].ToString();
+            }
+        }
