e052041b4973b9723be40e746dd5d784798f5687
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-15 11:16:03 +0700

    Filter data berdasarkan tanggal

--- SIBUK/Form4.cs
+++ SIBUK/Form4.cs
@@ -16,3 +16,16 @@ namespace SIBUK
         {
             dgvLaporan.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
+
+        private void btnTampil_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Transaksi WHERE tanggal BETWEEN @awal AND @akhir";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@awal", dtpAwal.Value.Date);
+                cmd.Parameters.AddWithValue("@akhir", dtpAkhir.Value.Date);
+
