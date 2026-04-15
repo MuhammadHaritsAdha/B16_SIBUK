77c62cfb67fb43fc14e388be4b106d6d527ca6be
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-15 11:15:12 +0700

    Setup datagridview

--- SIBUK/Form4.cs
+++ SIBUK/Form4.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SIBUK
+{
+    public partial class FormLaporan : Form
+    {
+        string connString = "Data Source=RITS;Initial Catalog=TokoBukuDB;Integrated Security=True";
+        public FormLaporan()
+        {
+            InitializeComponent();
+        }
+        private void FormLaporan_Load(object sender, EventArgs e)
+        {
+            dgvLaporan.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
