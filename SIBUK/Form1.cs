b0a0fef4e0bdcd847a311b439d1b183153dec327
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-15 10:34:32 +0700

    Membuat koneksi ke Database

--- SIBUK/Form1.cs
+++ SIBUK/Form1.cs
@@ -2,3 +2,10 @@
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+namespace SIBUK
+{
+    public partial class FormLogin : Form
+    {
+        SqlCommand cmd;
+        SqlConnection conn;
+        string connString = "Data Source=RITS;Initial Catalog=TokoBukuDB;Integrated Security=True";
