2d3cc847492807518197f84ec19742d1c1dece02
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-15 11:06:40 +0700

    setup FormKelolaBuku dan load data ke DataGridView

--- SIBUK/Form3.cs
+++ SIBUK/Form3.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SIBUK
+{
+    public partial class FormKelolaBuku : Form
+    {
+        string connString = "Data Source=RITS;Initial Catalog=TokoBukuDB;Integrated Security=True";
+        int selectedId = -1;
+        public FormKelolaBuku()
+        {
+            InitializeComponent();
+        }
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Buku";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvBuku.DataSource = dt;
+            }
+        }
+        private void FormKelolaBuku_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+            dgvBuku.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void dgvBuku_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvBuku.Rows[e.RowIndex];
+
+                selectedId = Convert.ToInt32(row.Cells["bukuId"].Value);
+
+                txtJudul.Text = row.Cells["judul"].Value.ToString();
+                txtPengarang.Text = row.Cells["pengarang"].Value.ToString();
+                txtPenerbit.Text = row.Cells["penerbit"].Value.ToString();
+                txtHarga.Text = row.Cells["hargaSatuan"].Value.ToString();
+                txtStok.Text = row.Cells["stok"].Value.ToString();
+            }
+        }
