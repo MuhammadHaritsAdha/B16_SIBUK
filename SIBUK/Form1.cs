e434208a8e045387fb16a7e277e5dd67148bf19a
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-15 10:52:23 +0700

    Membuat btnLogin dan Koneksi ke Database

--- SIBUK/Form1.cs
+++ SIBUK/Form1.cs
@@ -14,3 +14,37 @@ namespace SIBUK
             InitializeComponent();
             txtPassword.UseSystemPasswordChar = true;
         }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT * FROM Users WHERE username=@u AND password=@p";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@u", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@p", txtPassword.Text);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        string role = reader["role"].ToString();
+                        int userId = Convert.ToInt32(reader["userId"]);
+
+                        MessageBox.Show("Login berhasil!");
+
+                        FormTransaksi f = new FormTransaksi(role, userId);
+                        f.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Tidak Valid!");
+                    }
+                }
+            }
+
