942794f8eb08abc47f2a8ecf4ba13c888baff6f5
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-06-25 01:19:05 +0700

    Membuat class LaporanModel sebagai kolom data untuk CrystalReport

---
+++ SIBUK/LaporanModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIBUK
+{
+    internal class LaporanModel
+    {
+        public string transaksiId { get; set; }
+        public DateTime tanggal { get; set; }
+        public string judul { get; set; }
+        public int jumlah { get; set; }
+        public decimal hargaSatuan { get; set; }
+        public decimal totalHarga { get; set; }
+        public string statusBayar { get; set; }
+    }
+}
